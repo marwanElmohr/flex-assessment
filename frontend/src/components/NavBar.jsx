bb5dc816548090780ab6046723c1660819a2c9f5
marwanElmohr <marwankhalil2002@gmail.com>
2025-10-09 00:11:39 +0300

    edit first part in Home page

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,12 +1,14 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBuilding, faGrip } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBuilding, faGrip } from "@fortawesome/free-solid-svg-icons";
 import logo from "../images/image.webp";
 
 export default function NavBar() {
   return (
     <header className="sticky top-0 z-50 bg-[#284e4c] shadow py-4">
       <div className="container mx-auto flex justify-between items-center py-4 px-6 text-white">
-        <a href="#/" className="hover:cursor-pointer"><img src={firstImage} alt="logo" className='h-8 pl-12' /></a>
+        <a href="#/" className="hover:cursor-pointer">
+          <img src={logo} alt="logo" className="h-8 pl-12" />
+        </a>
         <nav className="space-x-8">
           <a href="#/dashboard" className="hover:cursor-pointer">
             <FontAwesomeIcon icon={faGrip} className="pr-2" />
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,119 +1,84 @@
-import TieredDiscounts from "../components/TieredDiscounts"; 
+import TieredDiscounts from "../components/TieredDiscounts";
 import firstImage from "../images/1stImage.webp";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faLocationDot,
+  faCalendar,
+  faUser,
+  faMinus,
+  faPlus,
+  faMagnifyingGlass,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function Home() {
-  const heroUrl =
-    "https://images.unsplash.com/photo-1505692794403-34d4982f88aa?q=80&w=2069&auto=format&fit=crop";
-
   return (
-    <>
+    <div>
       <section
-        className="h-[70vh] bg-cover bg-center flex items-center"
+        className="min-h-screen bg-cover bg-center flex items-center justify-center"
         style={{ backgroundImage: `url(${firstImage})` }}
       >
-        <div className=" backdrop-blur-sm p-8 max-w-2xl mx-8">
-          <h1 class="text-4xl lg:text-7xl xl:text-8xl font-bold text-white leading-tight">
-             <div>Book</div>
-            <div>Beautiful Stays</div>
-          </h1>
-          <form class="font-sans">
-            <div class="bg-[#FFF9E9] rounded-2xl p-2 lg:p-2 shadow-lg flex flex-row gap-1 sm:gap-2 md:gap-0 lg:gap-3">
-              
-              {</* cities */></>}
-              <div class="flex-1 border-gray-200 sm:pr-2 md:pr-4 lg:pr-8 ">
-                <div class="flex w-full items-center justify-between rounded-md py-4 text-xs md:text-lg lg:text-sm h-8 lg:h-8 px-1 md:px-4 lg:px-6 bg-transparent border-0 shadow-none">
-                  <div class="flex px-6 items-center gap-1 md:gap-3 lg:gap-5 px-0">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-map-pin h-3 sm:h-4 md:h-5 w-3 sm:w-4 md:w-5 text-gray-500 flex-shrink-0">
-                      <path d="M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0"></path>
-                      <circle cx="12" cy="10" r="3"></circle>
-                    </svg>
-                    <span>City</span>
-                    <div className="relative w-full px-12">
-                      <select
-                        className="apperance-none w-full rounded-md border-gray-200 bg-transparent text-gray-700 text-xs sm:text-sm md:text-lg focus:outline-none focus:ring-0"
-                        style={{
-                          textShadow: "0 0 0 transparent",
-                        }}
-                      >
-                        <option value="london">London, UK</option>
-                        <option value="paris">Paris, France</option>
-                        <option value="algiers">Algiers, Algeria</option>
-                        <option value="lisbon">Lisbon, Portugal</option>
-                      </select>
+        <div>
+          <div className="text-4xl lg:text-7xl xl:text-8xl font-bold text-white leading-tight my-4">
+            <h1>Book</h1>
+            <h1>Beautiful Stays</h1>
+          </div>
+          <form>
+            <div className="bg-[#FFF9E9] rounded-2xl p-6 shadow-lg flex flex-row gap-12 justify-start">
+              {/* cities */}
+              <div className="flex items-center justify-between">
+                <FontAwesomeIcon icon={faLocationDot} />
+                <select
+                  className="rounded-md border-gray-200 bg-transparent text-gray-700 focus:outline-none hover:cursor-pointer"
+                  defaultValue={"city"}
+                >
+                  <option value="city" disabled hidden>
+                    City
+                  </option>
+                  <option value="london">London, UK</option>
+                  <option value="paris">Paris, France</option>
+                  <option value="algiers">Algiera, Algeria</option>
+                  <option value="lisbon">Lisbon, Portugal</option>
+                </select>
+              </div>
 
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="20"
-                        height="20"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeWidth="2"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        className="lucide lucide-chevron-down absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 pointer-events-none"
-                      >
-                        <path d="m6 9 6 6 6-6" />
-                      </svg>
-                    </div>
-
-                  </div>
+              {/* dates */}
+              <div className="flex items-center justify-between border-l border-gray-200">
+                <div className="rounded-md px-4 bg-transparent hover:cursor-pointer">
+                  <FontAwesomeIcon icon={faCalendar} />
+                  <span className="text-gray-700 pl-2">Dates</span>
                 </div>
               </div>
 
-              {</* dates */></>}
-              <div class="flex-1 sm:border-r md:border-r border-gray-200 sm:px-2 md:px-4">
-                <div class="inline-flex items-center whitespace-nowrap rounded-md py-2 w-full h-8 sm:h-10 md:h-12 justify-start text-left font-normal text-xs sm:text-sm md:text-lg border-0 bg-transparent hover:bg-transparent px-1 sm:px-2 md:px-0">
-                  <div class="flex items-center gap-1 md:gap-3">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-calendar h-3 sm:h-4 md:h-5 w-3 sm:w-4 md:w-5 text-gray-500 flex-shrink-0">
-                      <path d="M8 2v4"></path>
-                      <path d="M16 2v4"></path>
-                      <rect width="18" height="18" x="3" y="4" rx="2"></rect>
-                      <path d="M3 10h18"></path>
-                    </svg>
-                    <span class="text-gray-700 truncate text-xs sm:text-sm md:text-base">Dates</span>
-                  </div>
-                </div>
+              {/* guests */}
+              <div className="flex items-center justify-between border-l border-gray-200">
+                <FontAwesomeIcon icon={faUser} className="px-2" />
+                <button
+                  className="rounded-full hover:bg-gray-100"
+                  type="button"
+                >
+                  <FontAwesomeIcon icon={faMinus} />
+                </button>
+                <span className="text-gray-700 px-2">1 Guest</span>
+                <button
+                  className="rounded-full hover:bg-gray-100"
+                  type="button"
+                >
+                  <FontAwesomeIcon icon={faPlus} />
+                </button>
               </div>
 
-              {</* guests */></>}
-              <div class="flex-1 sm:px-2 md:px-4 flex items-center justify-between">
-                <div class="flex items-center gap-1 md:gap-2">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user-round h-3 sm:h-4 md:h-5 w-3 sm:w-4 md:w-5 text-gray-500 flex-shrink-0">
-                    <circle cx="12" cy="8" r="5"></circle>
-                    <path d="M20 21a8 8 0 0 0-16 0"></path>
-                  </svg>
-                  <button class="inline-flex items-center justify-center text-xs h-5 w-5 sm:h-6 sm:w-6 md:h-8 md:w-8 p-0 rounded-full hover:bg-gray-100" type="button">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-minus h-2 w-2 sm:h-3 sm:w-3 md:h-4 md:w-4">
-                      <path d="M5 12h14"></path>
-                    </svg>
-                  </button>
-                  <span class="text-xs sm:text-sm md:text-lg text-gray-700 min-w-fit px-1">1 Guest</span>
-                  <button class="inline-flex items-center justify-center text-xs h-5 w-5 sm:h-6 sm:w-6 md:h-8 md:w-8 p-0 rounded-full hover:bg-gray-100" type="button">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-plus h-2 w-2 sm:h-3 sm:w-3 md:h-4 md:w-4">
-                      <path d="M5 12h14"></path>
-                      <path d="M12 5v14"></path>
-                    </svg>
-                  </button>
-                </div>
-              </div>
-
-              {</* Search Button */></>}
-              <div class="sm:pl-2 md:pl-4">
-                <button class="inline-flex items-center justify-center shadow py-2 w-auto h-8 sm:h-10 md:h-12 px-1 sm:px-4 md:px-8 bg-[#284E4C] hover:bg-[#1d3b39] text-white rounded-xl text-xs sm:text-sm md:text-lg font-medium" type="submit">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-search h-3 w-3 sm:h-4 sm:w-4 md:hidden">
-                    <circle cx="11" cy="11" r="8"></circle>
-                    <path d="m21 21-4.3-4.3"></path>
-                  </svg>
-                  <span class="hidden sm:inline md:inline">Search</span>
+              {/* Search Button */}
+              <div>
+                <button
+                  className="shadow py-2 px-1 sm:px-4 md:px-8 bg-[#284E4C] hover:bg-[#1d3b39] text-white rounded-xl"
+                  type="submit"
+                >
+                  <FontAwesomeIcon icon={faMagnifyingGlass} className="pr-2" />
+                  <span className="hidden sm:inline md:inline">Search</span>
                 </button>
               </div>
             </div>
           </form>
-
-
-          
-
         </div>
       </section>
 
@@ -146,7 +111,7 @@ export default function Home() {
           <TieredDiscounts />
         </section>
       </main>
-    </>
+    </div>
   );
 }
 
